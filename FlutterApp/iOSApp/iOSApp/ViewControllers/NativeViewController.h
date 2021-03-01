fbc9a6e9e805b81de850bc2e813bb68a7e3f2bc1
HCl <1580682761@qq.com>
2021-03-01 11:45:43 +0800

    Add files via upload

---
+++ FlutterApp/iOSApp/iOSApp/ViewControllers/NativeViewController.h
@@ -0,0 +1,18 @@
+//
+//  NativeViewController.h
+//  iOSApp
+//
+//  Created by HCl on 2021/2/26.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NativeViewController : UIViewController
+
+- (instancetype)initWithParams:(NSDictionary *)params;
+
+@end
+
+NS_ASSUME_NONNULL_END
